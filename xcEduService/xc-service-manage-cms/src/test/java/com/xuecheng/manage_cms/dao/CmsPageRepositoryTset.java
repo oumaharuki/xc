c00d624ea4d1cd7a1f40dd89ee34d00c6d29b369
moqinglong <ml@123456>
2019-10-18 19:03:45 +0800

    初始化

---
+++ xcEduService/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTset.java
@@ -0,0 +1,29 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @author
+ * @date 2019-10-18
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CmsPageRepositoryTset {
+
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+    @Test
+    public void testFindAll(){
+        List<CmsPage> all = cmsPageRepository.findAll();
+        System.out.println(all);
+
+
+    }
+}
